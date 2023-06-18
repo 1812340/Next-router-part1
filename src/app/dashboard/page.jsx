ba7cc486614423a6a8dc1dd69ed60ac75efa20c0
waqas <cs1812340@szabist.pk>
2023-06-18 23:29:53 +0500

    routing, file base routing, loading, css, modules css

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./page.module.css"
+
+const Dashbaord = () => {
+  return (
+    <div className={styles.container} >Dashbaord</div>
+  )
+}
+
+export default Dashbaord
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./page.module.css"
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div>2023 Waqas Ghori. All rights reserved.</div>
+      </div>
+  )
+}
+
+export default Footer
